c8eaf60ef731d745002b8ca0ca8f825fb449408c
Ashwin Patil <ashwinpatilofficial1997@gmail.com>
2023-04-30 20:53:54 -0400

    now user's q/a will be adde to FAQ's when the staff answers the question

--- src/main/java/service/AskQuestionService.java
+++ src/main/java/service/AskQuestionService.java
@@ -22,12 +22,29 @@ public class AskQuestionService {
 	public int AnswerQuestion(int qid, String staff_answer) {
 		
 		String ANSWER_QUESTION = "UPDATE user_qa SET status = \"answered\", staff_answer = \""+staff_answer+"\" where qid = "+qid+";";
+		String GET_QUESTION = "SELECT user_question from user_qa where qid ="+qid+";";
+		
 		int success = 0;
 		try {
 			DatabaseConnection db = new DatabaseConnection();	
 			Connection con = db.getConnection();
 			PreparedStatement preparedStatement = con.prepareStatement(ANSWER_QUESTION);
+			
 			success = preparedStatement.executeUpdate();
+			Statement statement = con.createStatement();
+			ResultSet rs = statement.executeQuery(GET_QUESTION);
+			String temp_question = "";
+			while(rs.next()) {
+				temp_question = rs.getString("user_question");
+			}
+			String INSERT_INTO_FAQ = "INSERT INTO questionandanswers (question,answer) VALUES (\""+temp_question+"\",\""+staff_answer+"\");";
+			PreparedStatement preparedStatement2 = con.prepareStatement(INSERT_INTO_FAQ);
+			success = preparedStatement.executeUpdate();
+			if(success > 0) {
+				//insert into FAQ
+				success = preparedStatement2.executeUpdate();
+			}
+			
 			db.closeConnection(con);
 		}catch(Exception e) {
 			e.printStackTrace();
